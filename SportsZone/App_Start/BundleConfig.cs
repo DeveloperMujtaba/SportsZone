d02c4a9404082223292ce33bb6e0d951ed208b09
Najam Khan <ghulammujtabakhan@outlook.com>
2021-01-06 15:15:31 +0500

    implemented new theme soccercity

--- SportsZone/App_Start/BundleConfig.cs
+++ SportsZone/App_Start/BundleConfig.cs
@@ -1,30 +1,70 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Web;
 using System.Web.Optimization;
 
 namespace SportsZone
 {
+    class NonOrderingBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+    static class BundleExtentions
+    {
+        public static Bundle NonOrdering(this Bundle bundle)
+        {
+            bundle.Orderer = new NonOrderingBundleOrderer();
+            return bundle;
+        }
+    }
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //js bundle
+            bundles.Add(new ScriptBundle("~/bundles/js").NonOrdering()
+            .Include(
+                "~/themes/default/js/vendor/jquery.js",
+                "~/themes/default/js/vendor/bootstrap.min.js",
+                "~/themes/default/js/gmap3.min.js",
+                "~/themes/default/js/bigslide.js",
+                "~/themes/default/js/slick.js",
+                "~/themes/default/js/waterwheelCarousel.js",
+                "~/themes/default/js/contact-form.js",
+                "~/themes/default/js/countTo.js",
+                "~/themes/default/js/datepicker.js",
+                "~/themes/default/js/rating-star.js",
+                "~/themes/default/js/range-slider.js",
+                "~/themes/default/js/spinner.js",
+                "~/themes/default/js/parallax.js",
+                "~/themes/default/js/countdown.js",
+                "~/themes/default/js/appear.js",
+                "~/themes/default/js/prettyPhoto.js",
+                "~/themes/default/js/wow-min.js",
+                "~/themes/default/js/main.js"
+                ));
+            // css bundle
+            bundles.Add(new StyleBundle("~/bundles/css").NonOrdering()
+                .Include(
+                "~/themes/default/css/bootstrap/bootstrap.min.css",
+                "~/themes/default/css/main.css",
+                "~/themes/default/css/icomoon.css",
+                "~/themes/default/css/animate.css",
+                "~/themes/default/css/transition.css",
+                "~/themes/default/css/font-awesome.min.css",
+                "~/themes/default/style.css",
+                "~/themes/default/css/color.css",
+                "~/themes/default/css/responsive.css"
+                ));
+            // mod js bundle
+            bundles.Add(new ScriptBundle("~/bundles/mod").NonOrdering()
+                .Include(
+                "~/themes/default/js/vendor/modernizr.js"
+                ));
+           // BundleTable.EnableOptimizations = true;
         }
     }
 }
